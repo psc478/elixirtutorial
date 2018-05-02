03c9a596a5f29d6976c8c47203a1010c9e2d1cf9
Patrick Creamer <patrickseancreamer@gmail.com>
2018-05-02 17:31:54 -0500

    Tutorial at 5:17 in Derek Banas video

--- elixirtut.ex
+++ elixirtut.ex
@@ -1,5 +1,13 @@
 defmodule M do
-  def hello do
-    IO.puts "Hello World"
+  def main do
+    #name = IO.gets("What is your name? ") |> String.trim
+    #IO.puts "Hello #{name}!"
+    data_stuff()
   end
+
+  def data_stuff do
+    my_int = 123
+    IO.puts "Integer (test change) #{is_integer(my_int)}"
+  end
+
 end
